0ffe372bfea3dfbda5d9c763c28e28175fe77a96
rosaauliaagustin <62470217+rosaauliaagustin@users.noreply.github.com>
2020-04-27 20:15:22 +0700

    Buat class main

---
+++ src/perulangan/Main.java
@@ -0,0 +1,19 @@
+package perulangan;
+
+public class Main {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        while(isiGelas != penuh){
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi Gelas sekarang : " + isiGelas + "ml");
+        }
+
+        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+}
