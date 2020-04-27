5a0f0e2f86af21586a72a2905e4096711abf1450
rosaauliaagustin <62470217+rosaauliaagustin@users.noreply.github.com>
2020-04-27 20:38:59 +0700

    Buat class tugas perulangan

---
+++ src/perulangan/TugasPerulangan.java
@@ -0,0 +1,17 @@
+package perulangan;
+
+import java.util.Scanner;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //Untuk mengimput nilai/data dari user
+
+        System.out.println ("-----|PENGURANGAN|-----");
+        System.out.print ("Masukkan Angka yang ingin di kurangkan = ");
+        int angka = input.nextInt();
+
+        for (int i = 1; i <= 10; ++i)
+            System.out.println (i+". Hasil dari Pengurangan "+i+" + "+angka+" = "+(i-angka));
+        
+    }
+}
