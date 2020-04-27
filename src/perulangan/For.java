8a9479b56ca4e7b84a071be52669236e21bba17e
rosaauliaagustin <62470217+rosaauliaagustin@users.noreply.github.com>
2020-04-27 20:34:25 +0700

    Buat class for

---
+++ src/perulangan/For.java
@@ -0,0 +1,20 @@
+package perulangan;
+
+public class For {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        for (isiGelas = 225; isiGelas <= penuh; isiGelas++) {
+            System.out.println("Sedang mengisi gelas...");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+
+        System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+
+}
+}
